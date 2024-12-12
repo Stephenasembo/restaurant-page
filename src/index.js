dc0d6c98d3fc92d594d9cc2eea65e67bf7401f3b
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-12 23:56:30 +0300

    Control Display Logic
    The module makes the code more organized

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import { homePara } from './initial-page'
 import { menuPara } from './menu';
 import { aboutPara } from './about';
+
+const content = document.querySelector('#content');
+
+const renderHtml = (function () {
+    const homePage = function(){
+        content.appendChild(homePara);
+    }
+
+    const menuPage = function(){
+        content.appendChild(menuPara);
+    }
+
+    const aboutPage = function(){
+        content.appendChild(aboutPara);
+    }
+
+
+    return {
+        homePage,
+        menuPage,
+        aboutPage,
+    }
+})()
+
