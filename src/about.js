b75b07a682e2dea8399b5b1342c0aae0bf2e7366
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-12 23:44:51 +0300

    Create Website Content

---
+++ src/about.js
@@ -0,0 +1,12 @@
+const aboutPara = document.createElement('p');
+
+aboutPara.innerText = `
+    Feel free to contact us at:
+    0700000111
+    0712345678
+    Rapster@gmail.com
+
+    Book a reservation withus through any form of contact
+`;
+
+export { aboutPara };
---
+++ src/initial-page.js
@@ -0,0 +1,10 @@
+const homePara = document.createElement('p');
+
+homePara.textContent = `
+            Located right at the heart of the city.
+            Come enjoy the ambience of Rapster Restaurant and fill up on our amazing dishes.
+            We serve both indegenous and exotic foods.
+            Pay us a visit and you are bound to enjoy.
+`
+
+export {homePara}
